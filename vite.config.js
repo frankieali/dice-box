c5e68bfb713abb1ba48a8ade09d559d22ac8239a
frankieali <frankieali4@gmail.com>
2021-09-01 22:53:16 -0400

    Lock in babylon version. Add rollup visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-// vite.config.js
+import { visualizer } from 'rollup-plugin-visualizer';
+
 module.exports = {
   base: process.env.NODE_ENV === 'production'
     ? './assets'
-    : '/'
+    : '/',
+	build: {
+		rollupOptions: {
+			plugins: [
+				visualizer({
+					open: true,
+					// template: "network",
+					gzipSize: true,
+					brotliSize: true
+				})
+			],
+		},
+	},
 }
